1424f3172c7c177e9f10c4e8634eebadab6ca81e
govind6498 <govindsingh6498@gmail.com>
2021-09-30 14:21:03 +0530

    Added:Find min and max value using stream

--- src/com/bridgelabz/streamapiexercise/StreamApi.java
+++ src/com/bridgelabz/streamapiexercise/StreamApi.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.streamapiexercise;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Function;
@@ -23,7 +24,7 @@ public class StreamApi {
 				.map(toDoubleFunction)
 				.collect(Collectors.toList());
 		System.out.println("Printing Double List: "+streamList);
-		
+
 		//Listing the first Even
 		Integer first = myNumberList.stream()
 				.filter(isEvenFunction)
@@ -31,5 +32,27 @@ public class StreamApi {
 				.findFirst()
 				.orElse(null);
 		System.out.println("First Even :"+first);
+
+		//Listing the find max number
+		Integer max = myNumberList.stream()
+				.filter(isEvenFunction)
+				.max(Comparator.comparing(Integer::intValue))
+				.orElse(null);
+		System.out.println("Max Value: "+max);
+
+		//Listing the find min number
+		Integer min = myNumberList.stream()
+				.filter(isEvenFunction)
+				.min(Comparator.comparing(Integer::intValue))
+				.orElse(null);
+		System.out.println("Min Value: "+min);
+		
+//		OR
+		Integer min1 = myNumberList.stream()
+				.filter(isEvenFunction)
+				.min((n1,n2)->n1-n2)
+				.orElse(null);
+		System.out.println("Min Value: "+min1);
+		
 	}
 }
