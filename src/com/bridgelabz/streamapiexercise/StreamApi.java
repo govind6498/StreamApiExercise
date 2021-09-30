1972d11146737831be23752773d5b34c643558fc
govind6498 <govindsingh6498@gmail.com>
2021-09-30 13:17:19 +0530

    Added:Ability to transform each element in to double using strem function

--- src/com/bridgelabz/streamapiexercise/StreamApi.java
+++ src/com/bridgelabz/streamapiexercise/StreamApi.java
@@ -1,17 +1,25 @@
 package com.bridgelabz.streamapiexercise;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class StreamApi {
 	public static void main(String[] args) {
 		List<Integer> myNumberList = new ArrayList<Integer>();
+		Function<Integer,Double> toDoubleFunction = Integer::doubleValue;
+
 		//Use Case 1
 		myNumberList.stream().forEach(n->{
 			System.out.println("Method1:Stream foreach value:"+n);
 		});
+		//use case 2
+		List<Double> streamList = myNumberList.stream()
+				.map(toDoubleFunction)
+				.collect(Collectors.toList());
+		System.out.println("Printing Double List: "+streamList);
 
 	}
 }
